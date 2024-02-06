5c5a423bef6daef28c24113d1857f873be7fddb8
Doyle Brown <dalpiad@gmail.com>
2024-02-05 18:27:32 -0600

    Completed Task 1.

--- queries.sql
+++ queries.sql
@@ -1,6 +1,14 @@
 --Part 1
+column_name     data_type
+id              int PK
+employer        varchar(255)
+name            varchar(255)
+skills          varchar(255)
 
 --Part 2
+
+
+
 
 --Part 3
 
