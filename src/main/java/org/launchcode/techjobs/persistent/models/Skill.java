75d2316decd90276c5afe3bdd9859ba088d7f9cb
Doyle Brown <dalpiad@gmail.com>
2024-02-05 19:28:05 -0600

    working on task 2.

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,38 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    public Skill() {};
+
+    @Size(max = 500, message = "Description too long!")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Skill skill = (Skill) o;
+        return Objects.equals(description, skill.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), description);
+    }
 }
